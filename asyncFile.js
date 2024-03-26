1871c552937023063f2fd250e40fba0dbbd1d647
Sarvanan N <nsaravanan1204@gmail.com>
2024-03-26 12:25:41 +0530

    Write File Async

--- asyncFile.js
+++ asyncFile.js
@@ -1,4 +1,10 @@
 const file = require('fs');
 
-file.readFile('./file.txt', 'utf-8', (err, data) => console.log(data));
+file.readFile('./file.txt', 'utf-8', (err, data) => {
+    console.log(data);
+    file.writeFile('./writeFile.txt', data + 'Write File Text', (err) => {
+        console.log('Your file is written');
+    })
+}
+);
 console.log("Will read file");
