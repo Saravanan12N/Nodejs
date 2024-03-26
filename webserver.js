b57378edcd227b0655ca7e479c59f3bba8daf808
Sarvanan N <nsaravanan1204@gmail.com>
2024-03-26 17:19:36 +0530

    Added json data

--- webserver.js
+++ webserver.js
@@ -1,13 +1,38 @@
 const http = require('http');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
 
-//Create the server and start the server
+//create the server
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!!!!');
+    const pathName = req.url;
+
+    if (pathName === '/overView') {
+        res.end('This is overview');
+    }
+    else if (pathName === '/product') {
+        res.end('This is the product');
+    } else if (pathName === '/api') {
+        fs.readFile('./dummyData.json', 'utf-8', (err, data) => {
+            res.writeHead(200, { 'Content-type': 'application/json' })
+            res.end(data);
+
+        })
+    }
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+
+        });
+        res.end('Page not found');
+    }
+
 })
+
+//start the server
 
 server.listen(8000, 'localhost', () => {
-    console.log("Server is running in port 8000");
+    console.log('Listen on port no 8000');
 })
-
 
